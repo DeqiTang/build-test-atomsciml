4b209e0c5f72217af3dace3f882966840bd1c4d4
Deqi Tang <deqitang@gmail.com>
2022-06-21 22:33:08 +0800

    build

---
+++ build_wheels/build_wheels_centos7.sh
@@ -0,0 +1,51 @@
+function install_dependencies() {
+local PARALLEL_NUM=$1
+# need working pip3 command
+yum install -y python3-pip
+# boost only is not enough. need boost-devel which contains headers and libraries needed
+# also for armadillo, you need armadillo-devel where -devel means develpment env including headers and libs
+yum install -y armadillo-devel 
+yum install -y atlas-devel blas lapack  # needed for python3.10 to build scipy
+yum install -y yaml-cpp-devel openssl-devel libssh2-devel
+
+# the boost-devel in centos 7 is too old
+yum install -y wget xz
+wget -c https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz
+tar -xf boost_1_78_0.tar.gz
+cd boost_1_78_0
+./bootstrap.sh
+./b2 install -j ${PARALLEL_NUM} --with-program_options --with-filesystem --with-system
+}
+
+function start_build() {
+local PARALLEL_NUM=$1
+for py in cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
+do
+/opt/python/${py}/bin/pip install --user scikit-build cython pybind11[global]
+done
+
+cd /root/atomsciml/
+for py in cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
+do
+# old build might destroy current build, so remove it
+rm -rf _skbuild
+/opt/python/${py}/bin/python3 setup.py build bdist_wheel -j ${PARALLEL_NUM}
+done
+
+for whl in /root/atomsciml/dist/atomsciml-*-linux_*.whl
+do
+auditwheel repair ${whl} --plat $PLAT -w /root/atomsciml/dist
+done
+}
+
+while getopts ":p:" arg; do
+case ${arg} in
+p)
+PARALLEL_NUM="${OPTARG}"
+;;
+esac
+done
+
+echo ${PARALLEL_NUM}
+install_dependencies ${PARALLEL_NUM}
+start_build ${PARALLEL_NUM}
