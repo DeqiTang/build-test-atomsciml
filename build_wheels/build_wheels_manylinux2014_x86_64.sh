cc2c55379c786f830c4b76c97d298c38eea8d53b
Deqi Tang <deqitang@gmail.com>
2022-07-09 00:33:07 +0800

    build

--- build_wheels/build_wheels_manylinux2014_x86_64.sh
+++ build_wheels/build_wheels_manylinux2014_x86_64.sh
@@ -1,5 +1,3 @@
-function install_dependencies() {
-local PARALLEL_NUM=$1
 yum install -y python3-pip
 yum install -y armadillo-devel 
 yum install -y openssl-devel
@@ -9,13 +7,11 @@ wget -c https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1
 tar -xf boost_1_78_0.tar.gz
 cd boost_1_78_0
 ./bootstrap.sh
-./b2 install -j ${PARALLEL_NUM} --with-program_options --with-filesystem --with-system
-}
+./b2 install -j 4 --with-program_options --with-filesystem --with-system
 
-function start_build() {
+
 # so that pybind11 can be detected by scikit-build
 export PATH=$PATH:${HOME}/.local/bin 
-local PARALLEL_NUM=$1
 for py in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
 do
 /opt/python/${py}/bin/pip install --user scikit-build cython pybind11[global]
@@ -25,23 +21,10 @@ cd /root/atomsciml/
 for py in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
 do
 rm -rf _skbuild
-/opt/python/${py}/bin/python3 setup.py build bdist_wheel -j ${PARALLEL_NUM}
+/opt/python/${py}/bin/python3 setup.py build bdist_wheel -j 4
 done
 
 for whl in /root/atomsciml/dist/atomsciml-*-linux_*.whl
 do
 auditwheel repair ${whl} -w /root/atomsciml/dist
 done
-}
-
-while getopts ":p:" arg; do
-case ${arg} in
-p)
-PARALLEL_NUM="${OPTARG}"
-;;
-esac
-done
-
-echo ${PARALLEL_NUM}
-install_dependencies ${PARALLEL_NUM}
-start_build ${PARALLEL_NUM}
--- build_wheels/build_wheels_manylinux_2_24_x86_64.sh
+++ build_wheels/build_wheels_manylinux_2_24_x86_64.sh
@@ -1,18 +1,3 @@
-function modify_apt_sources_list() {
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
-cat >/etc/apt/sources.list<<EOF
-deb http://mirrors.aliyun.com/debian stretch main contrib non-free
-deb http://mirrors.aliyun.com/debian stretch-proposed-updates main contrib non-free
-deb http://mirrors.aliyun.com/debian stretch-updates main contrib non-free
-deb http://mirrors.aliyun.com/debian-security/ stretch/updates main non-free contrib
-deb-src http://mirrors.aliyun.com/debian stretch main contrib non-free
-deb-src http://mirrors.aliyun.com/debian stretch-proposed-updates main contrib non-free
-deb-src http://mirrors.aliyun.com/debian stretch-updates main contrib non-free
-deb-src http://mirrors.aliyun.com/debian-security/ stretch/updates main non-free contrib
-EOF
-}
-
-function install_dependencies() {
 apt update
 apt install -y python3-pip
 apt install -y libarmadillo-dev 
@@ -20,12 +5,9 @@ apt install -y libboost-all-dev
 apt install -y --fix-missing
 apt install -y libboost-program-options-dev libboost-filesystem-dev libboost-system-dev
 apt install -y libtorch
-}
 
-function start_build() {
 # so that pybind11 can be detected by scikit-build
 export PATH=$PATH:${HOME}/.local/bin 
-local PARALLEL_NUM=$1
 for py in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
 do
 /opt/python/${py}/bin/pip3 install --user scikit-build cython pybind11[global]
@@ -35,33 +17,10 @@ cd /root/atomsciml/
 for py in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
 do
 rm -rf _skbuild
-/opt/python/${py}/bin/python3 setup.py bdist_wheel -j ${PARALLEL_NUM}
+/opt/python/${py}/bin/python3 setup.py bdist_wheel -j 4}
 done
 
 for whl in /root/atomsciml/dist/atomsciml*-linux_*.whl
 do
 auditwheel repair ${whl} -w /root/atomsciml/dist
 done
-
-}
-
-while getopts ":p:m:" arg; do
-case ${arg} in
-p)
-PARALLEL_NUM="${OPTARG}"
-;;
-m)
-MODIFY_APT_SOURCES="${OPTARG}"
-;;
-esac
-done
-
-echo ${PARALLEL_NUM}
-echo ${MODIFY_APT_SOURCES}
-
-if [[ ${MODIFY_APT_SOURCES} == *true* ]];then
-modify_apt_sources_list
-fi
-
-install_dependencies
-start_build ${PARALLEL_NUM}
