86bde97895e6d697efabc7b8d71ecde368d1ca95
Deqi Tang <deqitang@gmail.com>
2022-07-09 11:06:34 +0800

    build

--- build_wheels/build_wheels_manylinux2014_x86_64.sh
+++ build_wheels/build_wheels_manylinux2014_x86_64.sh
@@ -5,7 +5,7 @@
 
 yum install -y python3-pip
 yum install -y armadillo-devel 
-yum install -y openssl-devel
+# yum install -y openssl-devel
 
 yum install -y wget xz
 wget -c https://boostorg.jfrog.io/artifactory/main/release/1.79.0/source/boost_1_79_0.tar.gz
@@ -14,16 +14,15 @@ cd boost_1_79_0
 ./bootstrap.sh
 ./b2 install -j 4 --with-program_options --with-filesystem --with-system
 
-
 # so that pybind11 can be detected by scikit-build
 export PATH=$PATH:${HOME}/.local/bin 
-for py in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
+for py in cp38-cp38 cp39-cp39 cp310-cp310
 do
 /opt/python/${py}/bin/pip install --user scikit-build cython pybind11[global]
 done
 
 cd /root/atomsciml/
-for py in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
+for py in cp38-cp38 cp39-cp39 cp310-cp310
 do
 rm -rf _skbuild
 /opt/python/${py}/bin/python3 setup.py build bdist_wheel -j 4
--- build_wheels/build_wheels_manylinux_2_24_x86_64.sh
+++ build_wheels/build_wheels_manylinux_2_24_x86_64.sh
@@ -13,13 +13,13 @@ apt install -y libtorch
 
 # so that pybind11 can be detected by scikit-build
 export PATH=$PATH:${HOME}/.local/bin 
-for py in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
+for py in cp38-cp38 cp39-cp39 cp310-cp310
 do
 /opt/python/${py}/bin/pip3 install --user scikit-build cython pybind11[global]
 done
 
 cd /root/atomsciml/
-for py in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310
+for py in cp38-cp38 cp39-cp39 cp310-cp310
 do
 rm -rf _skbuild
 /opt/python/${py}/bin/python3 setup.py bdist_wheel -j 4
